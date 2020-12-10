4364625c70bf74c7d28cdff2d1817716fa9aa69d
manaka <romeod@gmail.com>
2020-12-10 15:43:57 +0100

    day 10 solution 2

--- 10_02.rb
+++ 10_02.rb
@@ -5,7 +5,7 @@ input_jolts = File.open('input_10_01.txt').read.split("\n").map(&:to_i).sort!
 
 hsh = {0 => 1}
 input_jolts.each do |el|
-  hsh[x] = ([hsh[el-1], hsh[el-2], hsh[el-3]] - [nil]).sum
+  hsh[el] = ([hsh[el-1], hsh[el-2], hsh[el-3]] - [nil]).sum
 end
 
 p hsh.values.last
